02c4338e5d32c7a442b42981fb58d2dbe3bb7749
Robert-Beane <beane031@morris.umn.edu>
2019-09-30 14:54:41 -0500

    Add tests for todo-list to gather valid and invalid information

---
+++ client/src/app/todos/todo-list.component.spec.ts
@@ -0,0 +1,122 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Observable} from 'rxjs';
+import { of } from 'rxjs';
+import {FormsModule} from '@angular/forms';
+
+import {CustomModule} from '../custom.module';
+
+import {Todo} from './todo';
+import {TodoListComponent} from './todo-list.component';
+import {TodoListService} from './todo-list.service';
+
+describe('Todo list', () => {
+
+  let todoList: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+
+  let todoListServiceStub: {
+    getTodos: () => Observable<Todo[]>
+  };
+
+  beforeEach(() => {
+    // stub TodoService for test purposes
+    todoListServiceStub = {
+      getTodos: () => of([
+        {
+          id: 'fry_id',
+          owner: 'Fry',
+          status: false,
+          body: 'Welcome to campus.',
+          category: 'school'
+        },
+        {
+          id: 'roberta_id',
+          owner: 'Roberta',
+          status: true,
+          body: 'We hope you like your stay.',
+          category: 'welcome'
+        },
+        {
+          id: 'worker_id',
+          owner: 'Worker',
+          status: true,
+          body: 'This sure is a lot of work.',
+          category: 'tasks'
+        }
+      ])
+    };
+
+    TestBed.configureTestingModule({
+      imports: [CustomModule],
+      declarations: [TodoListComponent],
+      // providers:    [ TodoListService ]  // NO! Don't provide the real service!
+      // Provide a test-double instead
+      providers: [{provide: TodoListService, useValue: todoListServiceStub}]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoListComponent);
+      todoList = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('contains all the todos', () => {
+    expect(todoList.todos.length).toBe(3);
+  });
+
+  it('contains a todo with the owner \'Roberta\'', () => {
+    expect(todoList.todos.some((todo: Todo) => todo.owner === 'Roberta')).toBe(true);
+  });
+
+  it('contain a todo with the owner \'Worker\'', () => {
+    expect(todoList.todos.some((todo: Todo) => todo.owner === 'Worker')).toBe(true);
+  });
+
+  it('doesn\'t contain a todo with the owner \'Darude\'', () => {
+    expect(todoList.todos.some((todo: Todo) => todo.owner === 'Darude')).toBe(false);
+  });
+
+  it('has two todos that have the status of true', () => {
+    expect(todoList.todos.filter((todo: Todo) => todo.status === true).length).toBe(2);
+  });
+});
+
+describe('Misbehaving Todo List', () => {
+  let todoList: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+
+  let todoListServiceStub: {
+    getTodos: () => Observable<Todo[]>
+  };
+
+  beforeEach(() => {
+    // stub TodoService for test purposes
+    todoListServiceStub = {
+      getTodos: () => Observable.create(observer => {
+        observer.error('Error-prone observable');
+      })
+    };
+
+    TestBed.configureTestingModule({
+      imports: [FormsModule, CustomModule],
+      declarations: [TodoListComponent],
+      providers: [{provide: TodoListService, useValue: todoListServiceStub}]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoListComponent);
+      todoList = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('generates an error if we don\'t set up a TodoListService', () => {
+    // Since the observer throws an error, we don't expect todos to be defined.
+    expect(todoList.todos).toBeUndefined();
+  });
+});

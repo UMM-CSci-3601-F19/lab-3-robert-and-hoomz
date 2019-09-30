f9285f0cbd760417fbf9501010f3550a23959f81
Robert-Beane <beane031@morris.umn.edu>
2019-09-29 22:15:40 -0500

    Add e2e tests for todo-list

---
+++ client/e2e/todo-list.e2e-spec.ts
@@ -0,0 +1,38 @@
+import {TodoPage} from './todo-list.po';
+import {browser, protractor} from "protractor";
+let origFn = browser.driver.controlFlow().execute;
+
+browser.driver.controlFlow().execute = function() {
+  let args = arguments;
+
+  origFn.call(browser.driver.controlFlow(), function () {
+    return protractor.promise.delayed(100);
+  });
+  return origFn.apply(browser.driver.controlFlow(), args);
+};
+
+// below are the tests
+
+describe('Todo list',  () =>{
+  let page: TodoPage;
+
+  beforeEach(()=>{
+    page = new TodoPage();
+  });
+
+  it('should get and highlight Todo Owner attribute', ()=>{
+    page.navigateTo();
+    expect(page.getTodoTitle()).toEqual('Todos'); // checks to see if page is called Todos
+  });
+
+  it('should type in owner box and see if its actually what it typed', ()=>{
+    page.navigateTo();
+    page.typeAOwner("ro");
+    expect(page.getUniqueTodo("video games")).toEqual("Roberta");
+    page.backspace();
+    page.backspace();
+    page.typeAOwner("wo");
+    expect(page.getUniqueTodo("homework")).toEqual("Workman");
+  });
+
+});

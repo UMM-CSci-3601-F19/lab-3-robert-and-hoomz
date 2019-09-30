a4509b2b6f3c0fb318515a3eed901a8f57e78212
Robert-Beane <beane031@morris.umn.edu>
2019-09-30 14:46:37 -0500

    Add tests for todo to gather valid and invalid information

--- client/src/app/todos/todo.component.spec.ts
+++ client/src/app/todos/todo.component.spec.ts
@@ -1,70 +1,70 @@
-// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-// import {Todo} from './todo';
-// import {TodoComponent} from './todo.component';
-// import {TodoListService} from './todo-list.service';
-// import {Observable} from 'rxjs';
-// import { of } from 'rxjs';
-//
-// describe('Todo component', () => {
-//
-//   let todoComponent: TodoComponent;
-//   let fixture: ComponentFixture<TodoComponent>;
-//
-//   let todoListServiceStub: {
-//     getTodoById: (todoId: string) => Observable<Todo>
-//   };
-//
-//   beforeEach(() => {
-//     // stub TodoService for test purposes
-//     todoListServiceStub = {
-//       getTodoById: (todoId: string) => of([
-//         // {
-//         //   owner: 'chris_id',
-//         //   name: 'Chris',
-//         //   age: 25,
-//         //   company: 'UMM',
-//         //   email: 'chris@this.that'
-//         // },
-//         // {
-//         //   id: 'pat_id',
-//         //   name: 'Pat',
-//         //   age: 37,
-//         //   company: 'IBM',
-//         //   email: 'pat@something.com'
-//         // },
-//         // {
-//         //   id: 'jamie_id',
-//         //   name: 'Jamie',
-//         //   age: 37,
-//         //   company: 'Frogs, Inc.',
-//         //   email: 'jamie@frogs.com'
-//         // }
-//       ].find(todo => todo.id === todoId))
-//     };
-//
-//     TestBed.configureTestingModule({
-//       declarations: [TodoComponent],
-//       providers: [{provide: TodoListService, useValue: todoListServiceStub}]
-//     });
-//   });
-//
-//   beforeEach(async(() => {
-//     TestBed.compileComponents().then(() => {
-//       fixture = TestBed.createComponent(TodoComponent);
-//       todoComponent = fixture.componentInstance;
-//     });
-//   }));
-//
-//   it('can retrieve Pat by ID', () => {
-//     todoComponent.setId('pat_id');
-//     expect(todoComponent.todo).toBeDefined();
-//     expect(todoComponent.todo.name).toBe('Pat');
-//     expect(todoComponent.todo.email).toBe('pat@something.com');
-//   });
-//
-//   it('returns undefined for Santa', () => {
-//     todoComponent.setId('Santa');
-//     expect(todoComponent.todo).not.toBeDefined();
-//   });
-//
-// });
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Todo} from './todo';
+import {TodoComponent} from './todo.component';
+import {TodoListService} from './todo-list.service';
+import {Observable} from 'rxjs';
+import { of } from 'rxjs';
+
+describe('Todo component', () => {
+
+  let todoComponent: TodoComponent;
+  let fixture: ComponentFixture<TodoComponent>;
+
+  let todoListServiceStub: {
+    getTodoById: (todoId: string) => Observable<Todo>
+  };
+
+  beforeEach(() => {
+    // stub TodoService for test purposes
+    todoListServiceStub = {
+      getTodoById: (todoId: string) => of([
+        {
+          id: 'fry_id',
+          owner: 'Fry',
+          status: false,
+          body: 'Welcome to campus.',
+          category: 'school'
+        },
+        {
+          id: 'roberta_id',
+          owner: 'Roberta',
+          status: true,
+          body: 'We hope you like your stay.',
+          category: 'welcome'
+        },
+        {
+          id: 'worker_id',
+          owner: 'Worker',
+          status: true,
+          body: 'This sure is a lot of work.',
+          category: 'tasks'
+        }
+      ].find(todo => todo.id === todoId))
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [TodoComponent],
+      providers: [{provide: TodoListService, useValue: todoListServiceStub}]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoComponent);
+      todoComponent = fixture.componentInstance;
+    });
+  }));
+
+  it('can retrieve Fry by ID', () => {
+    todoComponent.setId('fry_id');
+    expect(todoComponent.todo).toBeDefined();
+    expect(todoComponent.todo.owner).toBe('Fry');
+    expect(todoComponent.todo.category).toBe('school');
+  });
+
+  it('returns undefined for Pumpkin', () => {
+    todoComponent.setId('Pumpkin');
+    expect(todoComponent.todo).not.toBeDefined();
+  });
+
+});
